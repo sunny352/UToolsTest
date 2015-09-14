b5c39d54ee404894d9f9a18af9ce446b7054d5c5
sunny352@gmail.com <sunny352@gmail.com>
2015-09-14 15:25:33 +0800

    添加Combo计数功能测试场景

---
+++ Assets/TestScripts/TestCombo.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TestCombo : MonoBehaviour
+{
+	public Text text;
+	private ComboCounter m_counter;
+	void Start()
+	{
+		text.text = "无";
+
+		m_counter = new ComboCounter();
+		m_counter.Duration = 1.0f;
+		m_counter.onBegin = OnBegin;
+		m_counter.onIncrease = OnIncrease;
+		m_counter.onEnd = OnEnd;
+	}
+	public void Record()
+	{
+		m_counter.Record();
+	}
+	void FixedUpdate()
+	{
+		m_counter.FixedUpdate();
+	}
+	private void OnBegin()
+	{
+		Debug.LogFormat("OnBegin");
+	}
+	private void OnIncrease(int current)
+	{
+		text.text = string.Format("{0}", current);
+	}
+	private void OnEnd(int current)
+	{
+		text.text = "无";
+		Debug.LogFormat("OnEnd");
+	}
+}
---
+++ Assets/TestScripts/TestComboMono.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(ComboCounterMono))]
+public class TestComboMono : MonoBehaviour
+{
+	public Text text;
+	void Start()
+	{
+		text.text = "无";
+
+		ComboCounterMono mono = GetComponent<ComboCounterMono>();
+		mono.onBegin = OnBegin;
+		mono.onIncrease = OnIncrease;
+		mono.onEnd = OnEnd;
+	}
+	public void Record()
+	{
+		ComboCounterMono mono = GetComponent<ComboCounterMono>();
+		mono.Record();
+	}
+	private void OnBegin()
+	{
+		Debug.LogFormat("OnBegin");
+	}
+	private void OnIncrease(int current)
+	{
+		text.text = string.Format("{0}", current);
+	}
+	private void OnEnd(int current)
+	{
+		text.text = "无";
+		Debug.LogFormat("OnEnd");
+	}
+}
