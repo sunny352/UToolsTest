bef7b2d1087eb4305673677498bd9fc5374d7cdf
sunny352@gmail.com <sunny352@gmail.com>
2015-09-14 14:48:22 +0800

    添加链接多点的平滑曲线测试场景

---
+++ Assets/TestScripts/TestCurve.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCurve : MonoBehaviour
+{
+	public SlerpCurve curve;
+	private float m_startTime;
+	// Use this for initialization
+	void Start()
+	{
+		m_startTime = Time.time;
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		transform.position = curve.GetLinedPoints((Time.time - m_startTime) / 10.0f);
+	}
+}
