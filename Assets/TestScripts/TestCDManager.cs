b896e2e11f51fada414e8f921903446937d2e920
sunny352@gmail.com <sunny352@gmail.com>
2015-09-14 17:13:30 +0800

    添加CDManager的测试场景及代码

---
+++ Assets/TestScripts/TestCDManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCDManager : MonoBehaviour
+{
+	public bool[] CDList;
+	private CDManager m_cdManager;
+	// Use this for initialization
+	void Start()
+	{
+		m_cdManager = new CDManager();
+		m_cdManager.RegisterCD(1, 5, OnFinished, OnRefresh);
+		m_cdManager.RegisterCD(2, 10, OnFinished, OnRefresh);
+
+		CDList = new bool[m_cdManager.CDList.Count];
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		m_cdManager.OnFixedUpdate();
+		for (int index = 0; index < CDList.Length; ++index)
+		{
+			if (CDList[index])
+			{
+				if (m_cdManager.CDList[index].IsCDRunning)
+				{
+					continue;
+				}
+				else
+				{
+					m_cdManager.CDList[index].Start();
+				}
+			}
+			else
+			{
+				if (m_cdManager.CDList[index].IsCDRunning)
+				{
+					m_cdManager.CDList[index].Clear();
+				}
+				else
+				{
+					continue;
+				}
+			}
+		}
+	}
+	private void OnFinished(int id)
+	{
+		Debug.LogFormat("{0} is finished", id);
+		var cdList = m_cdManager.CDList;
+		for (int index = 0; index < CDList.Length; ++index)
+		{
+			if (cdList[index].ID == id)
+			{
+				CDList[index] = false;
+			}
+		}
+	}
+	private void OnRefresh(int id, float remain)
+	{
+
+	}
+}
---
+++ Assets/TestScripts/TestCDManagerMono.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CDManagerMono))]
+public class TestCDManagerMono : MonoBehaviour
+{
+	public bool[] CDList;
+	void Start()
+	{
+		CDList = new bool[GetComponent<CDManagerMono>().CDList.Length];
+	}
+
+	void FixedUpdate()
+	{
+		var cdList = GetComponent<CDManagerMono>().CDList;
+		for (int index = 0; index < CDList.Length; ++index)
+		{
+			if (CDList[index])
+			{
+				if (cdList[index].IsCDRunning)
+				{
+					continue;
+				}
+				else
+				{
+					cdList[index].Start();
+				}
+			}
+			else
+			{
+				if (cdList[index].IsCDRunning)
+				{
+					cdList[index].Clear();
+				}
+				else
+				{
+					continue;
+				}
+			}
+		}
+	}
+	void OnCDFinished(int id)
+	{
+		Debug.LogFormat("{0} is finished", id);
+		var cdList = GetComponent<CDManagerMono>().CDList;
+		for (int index = 0; index < CDList.Length; ++index)
+		{
+			if (cdList[index].ID == id)
+			{
+				CDList[index] = false;
+			}
+		}
+	}
+	void OnCDRefresh(string info)
+	{
+		//Debug.LogFormat("{0}", info);
+	}
+}
