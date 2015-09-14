0fd5c8ee2aa7229e6bfa5b833a19750c356cb3db
sunny352@gmail.com <sunny352@gmail.com>
2015-09-14 14:35:06 +0800

    添加弹力追踪测试场景及测试代码

---
+++ Assets/TestScripts/TestBounce.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBounce : MonoBehaviour
+{
+	public Transform Source;
+	public Transform Target;
+	public float Value = 5.0f;
+	public float MaxSpeed = 10.0f;
+	public float MinSpeed = 2.0f;
+	public bool IsEnable = true;
+	private Bounce m_bounce;
+	// Use this for initialization
+	void Start()
+	{
+		m_bounce = new Bounce();
+		m_bounce.Init(Source);
+		m_bounce.Target = Target;
+		m_bounce.Value = Value;
+		m_bounce.MaxSpeed = MaxSpeed;
+		m_bounce.MinSpeed = MinSpeed;
+	}
+	void FixedUpdate()
+	{
+		if (IsEnable)
+		{
+			m_bounce.Enable();
+		}
+		else
+		{
+			m_bounce.Disable();
+		}
+	}
+	void LateUpdate()
+	{
+		m_bounce.LateUpdate();
+	}
+}
---
+++ Assets/TestScripts/TestBounceMono.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBounceMono : MonoBehaviour
+{
+	public Transform Source;
+	public Transform Target;
+	public float Value = 5.0f;
+	public float MaxSpeed = 10.0f;
+	public float MinSpeed = 2.0f;
+	// Use this for initialization
+	void Start()
+	{
+		BounceMono mono = Source.gameObject.AddComponent<BounceMono>();
+		mono.Target = Target;
+		mono.Value = Value;
+		mono.MaxSpeed = MaxSpeed;
+		mono.MinSpeed = MinSpeed;
+	}
+}
