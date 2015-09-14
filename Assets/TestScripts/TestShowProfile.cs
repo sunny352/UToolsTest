81265e96faef3893bcb5c3bedf4fdb73780d4080
sunny352@gmail.com <sunny352@gmail.com>
2015-09-14 14:19:10 +0800

    添加ShowProfile测试

---
+++ Assets/TestScripts/TestShowProfile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestShowProfile : MonoBehaviour
+{
+	void Start()
+	{
+		ShowProfile.SampleTime = 1.0f;
+	}
+	void Update()
+	{
+		ShowProfile.Update();
+	}
+	void FixedUpdate()
+	{
+		ShowProfile.FixedUpdate();
+	}
+	void OnGUI()
+	{
+		ShowProfile.OnGUI();
+	}
+}
