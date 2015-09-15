491dc20ec6688058a8286248ca229a7a13b62703
sunny352@gmail.com <sunny352@gmail.com>
2015-09-15 13:35:32 +0800

    添加蓄力测试场景及代码

---
+++ Assets/TestScripts/TestChargeEnergy.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestChargeEnergy : MonoBehaviour
+{
+	private ChargeEnergy m_chargeEnergy;
+	void Start()
+	{
+		m_chargeEnergy = new ChargeEnergy();
+		m_chargeEnergy.IsFireOnEnd = true;
+		m_chargeEnergy.PreTime = 1.0f;
+		m_chargeEnergy.Duration = 1.0f;
+		m_chargeEnergy.MaxLevel = 5;
+
+		m_chargeEnergy.OnChargeStart = OnChargeStart;
+		m_chargeEnergy.OnChargeFire = OnChargeFire;
+		m_chargeEnergy.OnChargeLevelUp = OnChargeLevelUp;
+		m_chargeEnergy.OnChargeFinished = OnChargeFinished;
+		m_chargeEnergy.OnChargeInterupt = OnChargeInterupt;
+	}
+	void OnButtonClick()
+	{
+		Debug.Log("OnButtonClick");
+		if (!m_chargeEnergy.IsCharging)
+		{
+			m_chargeEnergy.OnBegin();
+		}
+		else
+		{
+			m_chargeEnergy.OnEnd();
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		m_chargeEnergy.FixedUpdate();
+	}
+
+	void OnChargeStart()
+	{
+		Debug.LogFormat("OnChargeStart");
+	}
+	void OnChargeFire(int level)
+	{
+		Debug.LogFormat("OnChargeFire {0}", level);
+	}
+	void OnChargeLevelUp(int level)
+	{
+		Debug.LogFormat("OnChargeLevelUp {0}", level);
+	}
+	void OnChargeFinished()
+	{
+		Debug.LogFormat("OnChargeFinished");
+	}
+	void OnChargeInterupt()
+	{
+		Debug.LogFormat("OnChargeInterupt");
+	}
+}
---
+++ Assets/TestScripts/TestChargeEnergyMono.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(ChargeEnergyMono))]
+public class TestChargeEnergyMono : MonoBehaviour
+{
+	void OnButtonClick()
+	{
+		Debug.Log("OnButtonClick");
+		if (!GetComponent<ChargeEnergyMono>().IsCharging)
+		{
+			gameObject.SendMessage("OnBegin");
+		}
+		else
+		{
+			gameObject.SendMessage("OnEnd");
+		}
+	}
+	void OnChargeStart()
+	{
+		Debug.LogFormat("OnChargeStart");
+	}
+	void OnChargeFire(int level)
+	{
+		Debug.LogFormat("OnChargeFire {0}", level);
+	}
+	void OnChargeLevelUp(int level)
+	{
+		Debug.LogFormat("OnChargeLevelUp {0}", level);
+	}
+	void OnChargeFinished()
+	{
+		Debug.LogFormat("OnChargeFinished");
+	}
+	void OnChargeInterupt()
+	{
+		Debug.LogFormat("OnChargeInterupt");
+	}
+}
