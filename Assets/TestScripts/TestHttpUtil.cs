5add514634a69ccbde8a9e244112ef9ed804f6b2
张明震 <sailorwork2015@gmail.com>
2017-03-17 18:19:09 +0800

    添加HttpUtil和ZipUtil的测试代码

---
+++ Assets/TestScripts/TestHttpUtil.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TestHttpUtil : MonoBehaviour
+{
+	void Start()
+	{
+		StartCoroutine(HttpUtil.Download("http://172.23.3.181/downloads/test.zip", "test.zip", HttpUtil.ENFileExist.enReplace, OnDownloading, OnFinished));
+	}
+
+	private void OnDownloading(int current, int total, float progress)
+	{
+		Debug.LogFormat("Downloaded : {0} Total : {1} Progress : {2}", current, total, progress);
+	}
+	private void OnFinished(int downloaded)
+	{
+		Debug.LogFormat("Downloaded : {0} Finished", downloaded);
+	}
+}
---
+++ Assets/TestScripts/TestZipUtil.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEngine;
+
+public class TestZipUtil : MonoBehaviour
+{
+	void Start()
+	{
+		Directory.CreateDirectory(Application.persistentDataPath + "/test");
+		using (StreamWriter writer = File.CreateText(Application.persistentDataPath + "/test/testfile.txt"))
+		{
+			writer.WriteLine("Just a test");
+		}
+		StartCoroutine(Test());
+	}
+	
+	IEnumerator Test()
+	{
+		yield return StartCoroutine(ZipUtil.Create("test"));
+		Debug.Log("Zip Finished");
+		yield return StartCoroutine(ZipUtil.Extract("test"));
+		Debug.Log("Extract Finished");
+	}
+}
